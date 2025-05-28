c01c7e7820a595152c61471df018d688f96b1436
Vixalie <vixalie@outlook.com>
2025-05-28 14:36:30 +0800

    feat(commands): 添加命令行工具帮助信息
    - 为 Config 命令添加描述："Config downloader."
    - 为 Download 命令添加描述："Analyze a model URL and download the model."
    - 为 Renew 命令添加描述："Renew locally saved model meta information."
    - 新增 List 命令并添加描述："List all models in current directory."

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,7 +6,12 @@ mod download;
 
 #[derive(Subcommand)]
 pub enum Commands {
+    #[command(about = "Config downloader.")]
     Config,
+    #[command(about = "Analyze a model URL and download the model.")]
     Download,
+    #[command(about = "Renew locally saved model meta information.")]
     Renew,
+    #[command(about = "List all models in current directory.")]
+    List,
 }
