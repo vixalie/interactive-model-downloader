311a021f14db1cc4804bc9cb6eda5c1736abc2c4
Vixalie <vixalie@outlook.com>
2025-06-25 15:50:14 +0800

    refactor(errors): 重构错误类型以提高可用性和可维护性
    - 移除了多个具体的解析错误类型，简化了错误分类
    - 新增了更具通用性的错误类型：MissingRequiredField、UnregconizedField和InvalidFieldValue
    - 这些更改使得错误处理更加灵活，易于理解和维护

--- src/errors.rs
+++ src/errors.rs
@@ -2,24 +2,12 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum CivitaiParseError {
-    #[error("Unknown model mode")]
-    UnknownModelMode,
-    #[error("Unknown model type")]
-    UnknownModelType,
-    #[error("Failed parsing model version file field: {0}")]
-    FailedParsingModelVersionFileField(String),
-    #[error("Failed parsing model version field: {0}")]
-    FailedParsingModelVersionField(String),
-    #[error("Failed retreiving model version field: {0}")]
-    FailedRetreivingModelVersionField(String),
-    #[error("Failed parsing model version file[{0}], field {1}")]
-    FailedParsingModelVersionFile(usize, String),
-    #[error("Failed parsing model field: {0}")]
-    FailedParsingModelField(String),
-    #[error("Failed retreiving model field: {0}")]
-    FailedRetreivingModelField(String),
-    #[error("Failed parsing version field {0} in model: {1}")]
-    FailedParsingVersionFieldInModel(usize, String),
+    #[error("Missing required field in {0}: {1}")]
+    MissingRequiredField(String, String),
+    #[error("Unregconized field in {0}: {1}")]
+    UnregconizedField(String, String),
+    #[error("Invalid field value in {0}: {1}")]
+    InvalidFieldValue(String, String),
     #[error("Universal error: {0}")]
     AnyhowError(anyhow::Error),
 }
