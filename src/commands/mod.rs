a6f1a198de1b087a8366bd0f87956a76e6af09b8
Vixalie <vixalie@outlook.com>
2025-06-29 17:28:55 +0800

    feat(commands): 添加模型元信息更新选项支持
    在Commands枚举中为Renew命令添加RenewOptions参数，使其能够接收更新选项

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,9 +3,11 @@ use clap::Subcommand;
 mod collector;
 mod config;
 mod download;
+mod renew;
 
 pub use config::process_config_options;
 pub use download::process_download_options;
+pub use renew::process_model_meta_renew;
 
 #[derive(Subcommand)]
 pub enum Commands {
@@ -14,7 +16,7 @@ pub enum Commands {
     #[command(about = "Analyze a model URL and download the model.")]
     Download(download::DownloadOptions),
     #[command(about = "Renew locally saved model meta information.")]
-    Renew,
+    Renew(renew::RenewOptions),
     #[command(about = "Scan all models in current directory, complete model meta information.")]
     Scan,
     #[command(about = "List all models in current directory.")]
